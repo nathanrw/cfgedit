9c9e58d88f915c2e0d79ec3c0117fde4f71ec78e
Nathan Woodward <nathanrichardwoodward@gmail.com>
2023-05-12 09:57:52 +0100

    Make it work

--- src/main.cpp
+++ src/main.cpp
@@ -2,118 +2,312 @@
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
-#include <stdio.h>
-#include <GLFW/glfw3.h> // Will drag system OpenGL headers
+#include <cstdio>
+#include <GLFW/glfw3.h>
+#include "rapidjson/rapidjson.h"
+#include "rapidjson/document.h"
+#include "rapidjson/filereadstream.h"
+#include "rapidjson/filewritestream.h"
+#include "rapidjson/prettywriter.h"
+#include "rapidjson/encodedstream.h"
+#include <vector>
+#include <string>
+#include <memory>
+
+struct file_deleter {
+    void operator()(FILE* file) { fclose(file); }
+};
+using file_ptr = std::unique_ptr<FILE, file_deleter>;
+
+class cfgedit {
+public:
+    cfgedit();
+    cfgedit(const cfgedit& that) = delete;
+    cfgedit& operator=(const cfgedit& that) = delete;
+    ~cfgedit();
+    void open_file(const char* path);
+    void gui();
+    void save();
+    void close();
+private:
+    void gui_for(const char* name, rapidjson::Value& value);
+    bool try_colour_gui_for(const char* name, rapidjson::Value& value);
+    std::string m_file_path;
+    rapidjson::Document m_open_document;
+};
+
+static void get_icon_rgba(unsigned char rgba[32*32*4])
+{
+    constexpr char* icon =
+        "00000000000000000000000000000000"
+        "00111111100111111100111111111100"
+        "00111111100111111100111111111100"
+        "00111000000111000000111000000000"
+        "00111000000111000000111000000000"
+        "00111000000111000000111000000000"
+        "00111000000111000000111000000000"
+        "00111000000111111100111001111100"
+        "00111000000111111100111001111100"
+        "00111000000111000000111000011100"
+        "00111000000111000000111000011100"
+        "00111000000111000000111000011100"
+        "00111000000111000000111000011100"
+        "00111111100111000000111111111100"
+        "00111111100111000000111111111100"
+        "00000000000000000000000000000000";
+    for (size_t i = 0; i < 32; ++i) {
+        for (size_t j = 0; j < 16; ++j) {
+            char value = icon[i+j*32] == '0' ? 0 : 0xff;
+            size_t row0 = j * 2 * 32 * 4;
+            size_t row1 = (j * 2 + 1) * 32 * 4;
+            rgba[i * 4 + row0 + 0] = 0;
+            rgba[i * 4 + row0 + 1] = 0;
+            rgba[i * 4 + row0 + 2] = 0;
+            rgba[i * 4 + row0 + 3] = value;
+            rgba[i * 4 + row1 + 0] = 0;
+            rgba[i * 4 + row1 + 1] = 0;
+            rgba[i * 4 + row1 + 2] = 0;
+            rgba[i * 4 + row1 + 3] = value;
+        }
+    }
+}
 
 static void glfw_error_callback(int error, const char* description)
 {
     fprintf(stderr, "GLFW Error %d: %s\n", error, description);
 }
 
+static void glfw_drop_callback(GLFWwindow* window, int count, const char** paths)
+{
+    if (count > 0) {
+        cfgedit* editor = (cfgedit*)glfwGetWindowUserPointer(window);
+        editor->open_file(paths[0]);
+    }
+}
+
+cfgedit::cfgedit()
+= default;
+
+cfgedit::~cfgedit()
+= default;
+
+void cfgedit::gui()
+{
+    ImGuiIO& io = ImGui::GetIO();
+    ImGui::SetNextWindowPos(ImVec2(0, 0));
+    ImGui::SetNextWindowSize(io.DisplaySize);
+    ImGui::Begin("cfgedit_window", NULL, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove);
+    if (m_open_document.HasParseError()) {
+        ImGui::Text("Parse error.");
+    } else if (m_open_document.IsNull()) {
+        ImGui::Text("Drag-drop a .json file onto this window to edit it.");
+    } else {
+        gui_for("Document", m_open_document);
+        if (ImGui::Button("Save")) {
+            save();
+        }
+        ImGui::SameLine();
+        if (ImGui::Button("Close")) {
+            close();
+        }
+    }
+    ImGui::End();
+}
+
+void cfgedit::gui_for(const char* name, rapidjson::Value& value)
+{
+    if (value.IsNull()) {
+        ImGui::Text("<NULL>");
+    } else if (value.IsBool()) {
+        bool bValue = value.GetBool();
+        if (ImGui::Checkbox(name, &bValue)) {
+            value.SetBool(bValue);
+        }
+    } else if (value.IsObject()) {
+        for (auto it = value.MemberBegin(); it != value.MemberEnd(); ++it) {
+            gui_for(it->name.GetString(), it->value);
+        }
+    } else if (value.IsArray()) {
+        try_colour_gui_for(name, value);
+    } else if (value.IsInt()) {
+        int iValue = value.GetInt();
+        if (ImGui::InputInt(name, &iValue)) {
+            value.SetInt(iValue);
+        }
+    } else if (value.IsLosslessDouble()) {
+        double dValue = value.GetDouble();
+        if (ImGui::InputDouble(name, &dValue)) {
+            value.SetDouble(dValue);
+        }
+    }
+}
+
+static bool str_ends_with(const char* str, const char* suffix)
+{
+    if (!str || !suffix) return false;
+    size_t str_len = strlen(str);
+    size_t suffix_len = strlen(suffix);
+    if (suffix_len > str_len) return false;
+    return strncmp(str + str_len - suffix_len, suffix, suffix_len) == 0;
+}
+
+bool cfgedit::try_colour_gui_for(const char* name, rapidjson::Value& value)
+{
+    bool ok = true;
+    ok = ok && (
+        str_ends_with(name, "Color") ||
+        str_ends_with(name, "Colour") ||
+        str_ends_with(name, "color") ||
+        str_ends_with(name, "colour")
+    );
+    ok = ok && value.IsArray();
+    ok = ok && (value.Size() == 3 || value.Size() == 4);
+    if (!ok) return false;
+    bool all_int = true;
+    bool all_double = true;
+    float rgba[4];
+    rgba[3] = 1;
+    for (size_t i = 0; i < value.Size(); ++i) {
+        all_int = all_int && value[i].IsInt();
+        all_int = all_int && 0 <= value[i].GetInt() && value[i].GetInt() <= 255;
+        all_double = all_double && value[i].IsDouble();
+        all_double = all_double && 0 <= value[i].GetDouble() && value[i].GetDouble() <= 1;
+    }
+    if (all_double) {
+        for (size_t i = 0; i < value.Size(); ++i) {
+            rgba[i] = (float) value[i].GetDouble();
+        }
+    }
+    else if (all_int) {
+        for (size_t i = 0; i < value.Size(); ++i) {
+            rgba[i] = value[i].GetInt() / 255.0f;
+        }
+    } else {
+        return false;
+    }
+    bool edited = false;
+    if (value.Size() == 3) {
+        edited = ImGui::ColorEdit3(name, rgba);
+    } else {
+        edited = ImGui::ColorEdit4(name, rgba);
+    }
+    if (edited) {
+        if (all_double) {
+            for (size_t i = 0; i < value.Size(); ++i) {
+                value[i].SetDouble(rgba[i]);
+            }
+        } else if (all_int) {
+            for (size_t i = 0; i < value.Size(); ++i) {
+                value[i].SetInt((int)(rgba[i] * 255));
+            }
+        }
+    }
+}
+
+void cfgedit::open_file(const char* path)
+{
+    m_file_path = path;
+    file_ptr fp(fopen(path, "rb"));
+    char buffer[256];
+    rapidjson::FileReadStream bis(fp.get(), buffer, sizeof(buffer));
+    rapidjson::AutoUTFInputStream<unsigned, rapidjson::FileReadStream> eis(bis);
+    m_open_document = {};
+    m_open_document.ParseStream<0, rapidjson::AutoUTF<unsigned>>(eis);
+}
+
+void cfgedit::save()
+{
+    if (m_file_path.empty()) return;
+    file_ptr fp(fopen(m_file_path.c_str(), "wb"));
+    char buffer[256];
+    rapidjson::FileWriteStream bos(fp.get(), buffer, sizeof(buffer));
+    using OutputStream = rapidjson::AutoUTFOutputStream<unsigned, rapidjson::FileWriteStream>;
+    OutputStream eos(bos, rapidjson::UTFType::kUTF8, true);
+    rapidjson::PrettyWriter<OutputStream, rapidjson::UTF8<>, rapidjson::AutoUTF<unsigned> > writer(eos);
+    writer.SetFormatOptions(rapidjson::PrettyFormatOptions::kFormatSingleLineArray);
+    m_open_document.Accept(writer);
+}
+
+void cfgedit::close()
+{
+    m_file_path = "";
+    m_open_document = {};
+}
+
 int main(int, char**)
 {
+    // Init glfw.
     glfwSetErrorCallback(glfw_error_callback);
     if (!glfwInit())
         return 1;
-
     const char* glsl_version = "#version 150";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-
-    // Create window with graphics context
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+OpenGL3 example", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "cfgedit", nullptr, nullptr);
     if (window == nullptr)
         return 1;
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1); // Enable vsync
 
-    // Setup Dear ImGui context
+    // Set icon.
+    unsigned char icon_rgba[32 * 32 * 4];
+    get_icon_rgba(icon_rgba);
+    GLFWimage icon_image = {};
+    icon_image.width = 32;
+    icon_image.height = 32;
+    icon_image.pixels = icon_rgba;
+    glfwSetWindowIcon(window, 1, &icon_image);
+
+    // Init imgui.
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGuiIO& io = ImGui::GetIO();
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-
-    // Setup Dear ImGui style
-    ImGui::StyleColorsDark();
-    //ImGui::StyleColorsLight();
-
-    // Setup Platform/Renderer backends
+    ImGui::StyleColorsLight();
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init(glsl_version);
 
-    // Our state
-    bool show_demo_window = true;
-    bool show_another_window = false;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    // Init cfgedit.
+    cfgedit editor;
+    glfwSetWindowUserPointer(window, &editor);
+    glfwSetDropCallback(window, glfw_drop_callback);
 
+    // Main loop.
     while (!glfwWindowShouldClose(window))
     {
-        // Poll and handle events (inputs, window resize, etc.)
-        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
-        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
-        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+        // Input.
         glfwPollEvents();
 
-        // Start the Dear ImGui frame
+        // Begin frame.
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
-        {
-            static float f = 0.0f;
-            static int counter = 0;
+        // Define gui.
+        editor.gui();
 
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-            ImGui::End();
-        }
-
-        // 3. Show another simple window.
-        if (show_another_window)
-        {
-            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
-            ImGui::End();
-        }
-
-        // Rendering
+        // Draw.
         ImGui::Render();
         int display_w, display_h;
         glfwGetFramebufferSize(window, &display_w, &display_h);
         glViewport(0, 0, display_w, display_h);
-        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+        glClearColor(0, 0, 0, 1);
         glClear(GL_COLOR_BUFFER_BIT);
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
         glfwSwapBuffers(window);
     }
 
-    // Cleanup
+    // Cleanup imgui.
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplGlfw_Shutdown();
     ImGui::DestroyContext();
 
+    // Cleanup glfw.
     glfwDestroyWindow(window);
     glfwTerminate();
 
+    // Done.
     return 0;
 }
